e3a056443fa73b9516257c3361febee13020b6b9
nagapurinarayana-ship-it <nagapuri.narayana@gmail.com>
2026-08-12 20:36:14 +0530

    Apply centralized route SEO metadata

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Nav from './Nav'
 import Footer from './Footer'
+import RouteSeo from './RouteSeo'
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col font-sans text-gray-900 bg-white">
+      <RouteSeo />
       <Nav />
       <main className="flex-1">
         <Outlet />
