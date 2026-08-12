5f5d477befbea31efcbdac009c9e4ad4406c713f
nagapurinarayana-ship-it <nagapuri.narayana@gmail.com>
2026-08-12 18:42:54 +0530

    fix: use runtime HelmetProvider package

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 import './index.css'
 
