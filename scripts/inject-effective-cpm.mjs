e0f41f0d8d41b426b1e212a121086e6fbfa950dd
nagapurinarayana-ship-it <nagapuri.narayana@gmail.com>
2026-08-12 21:15:46 +0530

    refactor: keep global EffectiveCPM scripts separate

--- scripts/inject-effective-cpm.mjs
+++ scripts/inject-effective-cpm.mjs
@@ -5,10 +5,7 @@ const DIST = resolve(process.cwd(), 'dist')
 const INDEX = resolve(DIST, 'index.html')
 
 const POPUNDER = '<script src="https://pl30815332.effectivecpmnetwork.com/98/cd/4d/98cd4d37b3a5c234c49c85952c033714.js"></script>'
-const NATIVE_SCRIPT = '<script async="async" data-cfasync="false" src="https://pl30815334.effectivecpmnetwork.com/29feded00f4ae2c8a3b2719189977fff/invoke.js"></script>'
 const SOCIAL_BAR = '<script src="https://pl30815335.effectivecpmnetwork.com/e7/87/aa/e787aa4e8d5075169853c0d1fe5fcabc.js"></script>'
-const BANNER = `<script>atOptions = {'key' : '75b0fc4d7ef9bda7dbda8e3863498abc', 'format' : 'iframe', 'height' : 60, 'width' : 468, 'params' : {}};</script><script src="https://www.highperformanceformat.com/75b0fc4d7ef9bda7dbda8e3863498abc/invoke.js"></script>`
-const SMARTLINK = 'https://www.effectivecpmnetwork.com/hcit0ft2?key=3383ae2b2a94f70103f6b28c372f4f72'
 
 const MARKER_START = '<!-- craftmypage-effectivecpm:start -->'
 const MARKER_END = '<!-- craftmypage-effectivecpm:end -->'
@@ -28,29 +25,6 @@ function inject(html) {
 
   const bodyBlock = `
 ${MARKER_START}
-<section class="cmp-monetization" aria-label="Advertisements">
-  <div class="cmp-ad-block">
-    <p class="cmp-ad-label">Advertisement</p>
-    <div class="cmp-ad-native" aria-label="Sponsored advertisement">
-      ${NATIVE_SCRIPT}
-      <div id="container-29feded00f4ae2c8a3b2719189977fff"></div>
-    </div>
-  </div>
-
-  <div class="cmp-ad-block">
-    <p class="cmp-ad-label">Advertisement</p>
-    <div class="cmp-ad-banner" aria-label="Sponsored advertisement">
-      ${BANNER}
-    </div>
-  </div>
-
-  <div class="cmp-ad-block cmp-smartlink">
-    <p class="cmp-ad-label">Sponsored</p>
-    <a href="${SMARTLINK}" target="_blank" rel="sponsored noopener noreferrer">
-      Explore sponsored offers
-    </a>
-  </div>
-</section>
 ${SOCIAL_BAR}
 ${MARKER_END}
 </body>`
@@ -59,10 +33,10 @@ ${MARKER_END}
 }
 
 if (!existsSync(INDEX)) {
-  throw new Error(`Cannot inject EffectiveCPM ads: ${INDEX} does not exist. Run the Vite build first.`)
+  throw new Error(`Cannot inject EffectiveCPM global scripts: ${INDEX} does not exist. Run the Vite build first.`)
 }
 
 const html = readFileSync(INDEX, 'utf8')
 const updated = inject(html)
 writeFileSync(INDEX, updated, 'utf8')
-console.log('EffectiveCPM monetization injected into dist/index.html')
+console.log('EffectiveCPM global scripts injected into dist/index.html')
