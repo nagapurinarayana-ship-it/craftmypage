15a648f1936c4a874f85f18afdd1e4badee013b6
nagapurinarayana-ship-it <nagapuri.narayana@gmail.com>
2026-08-12 20:37:36 +0530

    Avoid preloading route-only editor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    modulePreload: {
+      resolveDependencies: (_filename, deps) =>
+        deps.filter((dep) => !/ResumeBuilderPage-|PDFButton-/.test(dep)),
+    },
+  },
 })
