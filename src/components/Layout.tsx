4b7ca737a5edcca532d7ad2faefc1909f39585ba
nagapurinarayana-ship-it <nagapuri.narayana@gmail.com>
2026-08-26 18:53:51 +0100

    Add existing ad units to non-home routes (#19)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Nav from './Nav'
 import Footer from './Footer'
 import RouteSeo from './RouteSeo'
 import RouteSeoDynamic from './RouteSeoDynamic'
+import EffectiveCpmHomeAds from './monetization/EffectiveCpmHomeAds'
 
 export default function Layout() {
+  const location = useLocation()
+  const isHome = location.pathname === '/'
+
   return (
     <div className="min-h-screen flex flex-col font-sans text-gray-900 bg-white">
       <RouteSeo />
@@ -13,6 +17,11 @@ export default function Layout() {
       <main className="flex-1">
         <Outlet />
       </main>
+      {!isHome && (
+        <div className="cmp-container py-10 sm:py-12">
+          <EffectiveCpmHomeAds />
+        </div>
+      )}
       <Footer />
     </div>
   )
